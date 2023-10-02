7e0c27665b8de516b51e36a77c3c4741490f3ab3
Emanuele Tinari <82208785+EmanueleTinari@users.noreply.github.com>
2023-10-02 14:12:39 +0200

    Create Elimina link Recenti.ps1
    Questo Script in PowerShell eliminerà tutti i link (*.lnk) presenti in alcune cartelle Recent di Windows. Funziona si col 10 che con l'11.

---
+++ Elimina link Recenti.ps1
@@ -0,0 +1,8 @@
+Get-ChildItem $env:APPDATA\Microsoft\Windows\Recent\* -File -Force -Exclude desktop.ini | 
+Remove-Item -Force -ErrorAction SilentlyContinue
+Get-ChildItem $env:APPDATA\Microsoft\Windows\Recent\AutomaticDestinations\* -File -Force -Exclude desktop.ini, f01b4d95cf55d32a.automaticDestinations-ms | 
+Remove-Item -Force -ErrorAction SilentlyContinue
+Get-ChildItem $env:APPDATA\Microsoft\Windows\Recent\CustomDestinations\* -File -Force -Exclude desktop.ini | 
+Remove-Item -Force -ErrorAction SilentlyContinue
+Get-ChildItem $env:APPDATA\Microsoft\Office\Recent\* -File -Force -Exclude desktop.ini |
+Remove-Item -Force -ErrorAction SilentlyContinue
