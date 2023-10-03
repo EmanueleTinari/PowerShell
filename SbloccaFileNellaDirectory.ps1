38c1716618b8369410038df1065afb0e179e892e
Emanuele Tinari <82208785+EmanueleTinari@users.noreply.github.com>
2023-10-03 21:18:50 +0200

    Create SbloccaFileNellaDirectory.ps1
    Lo script apre una finestra per selezionare una cartella in cui si vuole eseguire lo stesso. Una volta selezionata, sblocca (un-lock) tutti i files presenti in essa.

---
+++ SbloccaFileNellaDirectory.ps1
@@ -0,0 +1,19 @@
+﻿Function Get-Folder($initialDirectory="")
+
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Seleziona una cartella"
+    $foldername.rootfolder = "MyComputer"
+    $foldername.SelectedPath = $initialDirectory
+
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
+
+$a = Get-Folder
+dir $a | unblock-file -confirm
